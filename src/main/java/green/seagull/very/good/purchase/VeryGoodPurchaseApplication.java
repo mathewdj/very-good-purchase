9667e05196b080748c5cbdbad5dbd8d11fc6dffc
green-seagull <9652156+green-seagull@users.noreply.github.com>
2021-09-17 13:22:17 +1000

    Initial project generated from Spring initialiser

---
+++ src/main/java/green/seagull/very/good/purchase/VeryGoodPurchaseApplication.java
@@ -0,0 +1,13 @@
+package green.seagull.very.good.purchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeryGoodPurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeryGoodPurchaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/green/seagull/very/good/purchase/VeryGoodPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package green.seagull.very.good.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeryGoodPurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
