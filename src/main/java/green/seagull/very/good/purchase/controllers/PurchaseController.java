709ba381cf4d75f8a9695b022bb5898db4eae00f
mathewdj <9652156+green-seagull@users.noreply.github.com>
2021-12-19 20:58:57 +1100

    Refactor with PurchaseService

--- src/main/java/green/seagull/very/good/purchase/controllers/PurchaseController.java
+++ src/main/java/green/seagull/very/good/purchase/controllers/PurchaseController.java
@@ -1,7 +1,7 @@
 package green.seagull.very.good.purchase.controllers;
 
 import green.seagull.very.good.purchase.domain.Purchase;
-import green.seagull.very.good.purchase.repository.PurchaseCrudRepository;
+import green.seagull.very.good.purchase.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -13,18 +13,18 @@ import reactor.core.publisher.Mono;
 @CrossOrigin
 @RequestMapping(path = "/api/purchases")
 public class PurchaseController {
-
+    
     @Autowired
-    private PurchaseCrudRepository purchaseCrudRepository;
+    private PurchaseService purchaseService;
 
     @GetMapping()
     public @ResponseBody Flux<Purchase> getAllPurchases() {
-        return purchaseCrudRepository.findAll();
+        return purchaseService.findAll();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public @ResponseBody Mono<Purchase> save(@RequestBody Purchase purchase) {
-        return purchaseCrudRepository.save(purchase);
+        return purchaseService.save(purchase);
     }
 }
