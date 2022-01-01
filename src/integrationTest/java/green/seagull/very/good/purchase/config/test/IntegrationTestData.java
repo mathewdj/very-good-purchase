22e6299ffe99e0e6267ad19542515349594698e1
mathewdj <9652156+green-seagull@users.noreply.github.com>
2022-01-01 14:19:17 +1100

    Fix broken integration test post kotlin conversion

--- src/integrationTest/java/green/seagull/very/good/purchase/config/test/IntegrationTestData.java
+++ src/integrationTest/java/green/seagull/very/good/purchase/config/test/IntegrationTestData.java
@@ -8,13 +8,15 @@ import java.time.LocalDate;
 
 public class IntegrationTestData {
     public static Purchase bookPurchase() {
-        return new Purchase("Tales of Beedle the Bard",
+        return new Purchase(null,
+                "Tales of Beedle the Bard",
                 LocalDate.of(2017, 1, 1),
                 PurchaseType.Book, BigDecimal.TEN);
     }
 
     public static Purchase ps4Purchase() {
-        return new Purchase("Normand Salazar",
+        return new Purchase(null,
+                "Normand Salazar",
                 LocalDate.of(2021, 9, 20),
                 PurchaseType.PS4, BigDecimal.ONE);
     }
